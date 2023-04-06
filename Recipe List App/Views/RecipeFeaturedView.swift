09bc0fb6a5fd740d8872123e5a09bfa59dcbed15
Colstin Donaldson <colstin50@gmail.com>
2023-04-06 10:09:54 -0700

    Modify RecipeFeaturedView to fix the index bug by using the filter method to create a filtered array of featured items then we just looped through that

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -13,7 +13,12 @@ struct RecipeFeaturedView: View {
     @State var isDetailViewShowing = false
     @State var tabSelectedIndex = 0
     
+    
+    
     var body: some View {
+        
+        let featuredRecipes = model.recipes.filter({ $0.featured })
+        
         
         
         VStack(alignment: .leading, spacing: 0) {
@@ -27,38 +32,33 @@ struct RecipeFeaturedView: View {
             GeometryReader { geo in
                 TabView(selection: $tabSelectedIndex){
                     //Loop through each recipe from the json data
-                    ForEach(0..<model.recipes.count, id: \.self){ index in
-                        
-                        if model.recipes[index].featured == true{
-                           
-                            Button{
-                                self.isDetailViewShowing = true
-                            } label: {
-                                ZStack {
-                                    Rectangle()
-                                        .foregroundColor(Color.white)
+                    ForEach(0..<featuredRecipes.count, id: \.self){ index in
 
-                                    VStack(spacing: 0){
-                                        Image(model.recipes[index].image)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .clipped()
-                                        Text(model.recipes[index].name)
-                                            .padding(5)
-                                    }
+                        //MARK: Recipe Card Button
+                        Button{
+                            self.isDetailViewShowing = true
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(Color.white)
+
+                                VStack(spacing: 0){
+                                    Image(featuredRecipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    Text(featuredRecipes[index].name)
+                                        .padding(5)
                                 }
                             }
-                            .tag(index)
-                            .sheet(isPresented: $isDetailViewShowing){
-                                RecipeDetailView(recipe: model.recipes[index])
-                            }
-                            
-                            .buttonStyle(PlainButtonStyle())
-                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
-                            .cornerRadius(15)
-                            .shadow(color: Color(.sRGB,red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
-                        
                         }
+                        .tag(index)
+                        .buttonStyle(PlainButtonStyle())
+                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                        .cornerRadius(15)
+                        .shadow(color: Color(.sRGB,red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                        
+                        
                     }
                 }
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
@@ -68,25 +68,21 @@ struct RecipeFeaturedView: View {
             VStack(alignment: .leading, spacing: 10){
                 Text("Prep Time:")
                     .font(Font.custom("Avenir Heavy", size: 16))
-                Text(model.recipes[tabSelectedIndex].prepTime)
+                Text(featuredRecipes[tabSelectedIndex].prepTime)
                 Text("highlights")
                     .font(Font.custom("Avenir Heavy", size: 16))
-                RecipeHighlights(highlights: model.recipes[tabSelectedIndex].highlights)
+                RecipeHighlights(highlights: featuredRecipes[tabSelectedIndex].highlights)
             }
             .padding([.leading, .bottom])
         }
+            .sheet(isPresented: $isDetailViewShowing){
+            RecipeDetailView(recipe: featuredRecipes[tabSelectedIndex])
+        }
             .font(Font.custom("Avenir", size: 15))
-        .onAppear {
+        /*.onAppear {
             setFeaturedIndex()
-        }
+        }*/
     }//body
-    
-    func setFeaturedIndex(){
-        let index = model.recipes.firstIndex { recipe in
-           return recipe.featured
-        }
-        tabSelectedIndex = index ?? 0
-    }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
